be6df72e14a75946dca5f7221b6cea5950c45f2c
hyojeongchoi <63584457+hyojeongchoi@users.noreply.github.com>
2024-05-03 16:53:49 +0900

    [PGS] k번째 수 / Level 1

---
+++ hyojeongchoi/sort/k번째 수.py
@@ -0,0 +1,7 @@
+def solution(array, commands):
+    answer = []
+    for i in range(len(commands)):
+        arr = array[commands[i][0]-1:commands[i][1]]
+        arr.sort()
+        answer.append(arr[commands[i][2]-1])
+    return answer
