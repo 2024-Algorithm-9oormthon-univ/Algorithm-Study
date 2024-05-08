fd7b8f7a46ba168c201cf2bf0fca38343bdd4a46
김동균 <wooheuy5752@nate.com>
2024-05-08 17:36:34 +0900

    [PGS] 입국심사 / Level3

---
+++ donggyun/binary_search/입국심사.js
@@ -0,0 +1,23 @@
+function solution(n, times) {
+  times.sort((a, b) => a - b);
+  let left = 1;
+  let right = n * times[times.length - 1];
+  let answer = right;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    let count = 0;
+    times.forEach((value) => {
+      count += Math.floor(mid / value);
+      if (count >= n) {
+        answer = Math.min(mid, answer);
+        return;
+      }
+    });
+    if (count >= n) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return answer;
+}
