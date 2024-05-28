88ea9eba219244f94f72d6b97d353d51c4e6d764
Ahyeon, Jung <75254185+a-honey@users.noreply.github.com>
2024-05-28 16:06:22 +0900

    [PGS] 사칙연산 / 입문

---
+++ a-honey/입문/1_사칙연산.java
@@ -0,0 +1,27 @@
+class Solution {
+  public int solution(int num1, int num2) {
+    int answer = num1 + num2;
+    return answer;
+  }
+}
+
+class Solution {
+  public int solution(int num1, int num2) {
+    int answer = num1 - num2;
+    return answer;
+  }
+}
+
+class Solution {
+  public int solution(int num1, int num2) {
+    int answer = num1 * num2;
+    return answer;
+  }
+}
+
+class Solution {
+  public int solution(int num1, int num2) {
+    int answer = num1 / num2;
+    return answer;
+  }
+}
