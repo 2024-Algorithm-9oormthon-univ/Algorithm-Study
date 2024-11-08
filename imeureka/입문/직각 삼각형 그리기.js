d72e729da5717ead7cc3bc9915648514cd5db883
lky062@naver.com <lky062@naver.com>
2024-11-08 20:41:22 +0900

    [PGS] 직각삼각형 그리기 / level 0

--- imeureka/입문/직각 삼각형 그리기.js
+++ imeureka/입문/직각 삼각형 그리기.js
@@ -13,14 +13,11 @@ let input = [];
 
 // 3. [이벤트는 사용자가 Enter 키를 눌렀을 때 발생]
 // 4. [close 이벤트는 readline 인터페이스가 close 이벤트를 감지할 때마다 실행되는 콜백 함수를 정의합니다.]
-rl.on("line", function (line) {
-	input = line.split(" ");
-}).on("close", function () {
-	solution(Number(input[0]));
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+   const n = Number(input[0]);
+    for(let i = 1; i<=n; i++){
+        console.log("*".repeat(i))
+    }
 });
-
-function solution(n) {
-	for (let i = 1; i < n + 1; i++) {
-		console.log("*".repeat(i));
-	}
-}
