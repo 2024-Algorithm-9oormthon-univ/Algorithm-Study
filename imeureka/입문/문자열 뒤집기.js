77591eb46b636f8b9377282210ba809a459aa9ff
lky062@naver.com <lky062@naver.com>
2024-11-08 20:24:17 +0900

    [PGS] 문자열 뒤집기  /  level 0

--- imeureka/입문/문자열 뒤집기.js
+++ imeureka/입문/문자열 뒤집기.js
@@ -2,15 +2,28 @@
  * 1. 문자열 뒤집을 때 필요한 메소드 :
  *    reverse() , split(), join()
  * 2. 스프레드 연산자는 spilt()메소드를 대체할 수 있다.
+ *    split은 문자열을 쪼개 배열로 만든다.
+ *    스프레드는 배열 또는 문자열을 개별 요소로 펼친다.
  */
+
 function reverseString(str) {
 	let splitString = str.split("");
 	// ["H","I"]
 	let reverseArray = splitString.reverse();
 	let joinArray = reversArray.join("");
+	return joinArray;
 }
-// 인줄 알았으나 실패
+
 function solution(my_string) {
 	let answer = [...my_string].reverse().join("");
 	return answer;
+}
+
+/**
+ * 복습
+ */
+
+function solution(my_string) {
+	let reverseString = [...my_string].reverse().join();
+	return reverseString;
 }
